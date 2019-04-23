1f0b238de9655974cb22300afa76bf5be49d1584
Cameron Auld <cga51@uclive.ac.nz>
2019-04-23 13:07:07 +1200

    Create skeleton of main views (xml) - Added Play and Tutorial buttons to MainActivity (home view) - Added strings for buttons and app name

--- app/src/main/java/me/olliechick/stilluntitled/MainActivity.kt
+++ app/src/main/java/me/olliechick/stilluntitled/MainActivity.kt
@@ -2,11 +2,25 @@ package me.olliechick.stilluntitled
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to buttons
+        val playButton = findViewById(R.id.play_button) as Button
+        val helpButton = findViewById(R.id.help_button) as Button
+
+        // set on-click listeners
+        playButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "TODO: Load game instance.", Toast.LENGTH_SHORT).show()
+        }
+        helpButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "TODO: Load help.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
