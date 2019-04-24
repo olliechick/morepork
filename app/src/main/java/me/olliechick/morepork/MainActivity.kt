45b130739e3ad8e281c97a633e0a4d8d3122dd23
Ollie Chick <och26@uclive.ac.nz>
2019-04-24 22:03:44 +1200

    remove testing log message

--- app/src/main/java/me/olliechick/morepork/MainActivity.kt
+++ app/src/main/java/me/olliechick/morepork/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
@@ -37,7 +36,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        Log.i("FOO", "Creating options menu")
         val inflater = menuInflater
         inflater.inflate(R.menu.mainmenu, menu)
         return true
