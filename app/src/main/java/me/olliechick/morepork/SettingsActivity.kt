c284bd861b7c1e7eb392f7dba157559521ecefe9
Ollie Chick <och26@uclive.ac.nz>
2019-05-10 15:12:43 +1200

    add support button to settings

--- app/src/main/java/me/olliechick/morepork/SettingsActivity.kt
+++ app/src/main/java/me/olliechick/morepork/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package me.olliechick.morepork
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
@@ -19,6 +22,17 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            initSettings()
+        }
+
+        private fun initSettings() {
+            findPreference<Preference>("feedback")?.setOnPreferenceClickListener {
+                val intent = Intent(Intent.ACTION_SENDTO)
+                intent.data = Uri.parse("mailto:" + getString(R.string.dev_email))
+                intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name))
+                startActivity(intent)
+                true
+            }
         }
     }
 }
