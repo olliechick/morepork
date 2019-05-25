592137791a7c7d7be661c3ad18a2fd4ac2d8590b
Cameron Auld <cga51@uclive.ac.nz>
2019-05-25 14:34:35 +1200

    Add obstacles and Connecting controls to make icon move up and down - Added methods for drawing obstacles on top of background. - Added methods for drawing owl on background in correct location.

--- app/src/main/java/nz/co/olliechick/morepork/SideScrollView.kt
+++ app/src/main/java/nz/co/olliechick/morepork/SideScrollView.kt
@@ -1,49 +1,92 @@
 package nz.co.olliechick.morepork
 
 import android.content.Context
-import android.content.SharedPreferences
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-import androidx.preference.PreferenceManager
 import java.util.ArrayList
 
 /**
  *  Handles animating background images.
  */
-class SideScrollView internal constructor(context: Context, screenWidth: Int, screenHeight: Int) :
-    SurfaceView(context), Runnable {
+class SideScrollView internal constructor(internal var context: Context, var screenWidth: Int,  var screenHeight: Int) : SurfaceView(context), Runnable {
 
-    private var backgrounds: ArrayList<Background> =
-        arrayListOf(Background(this.context, screenWidth, screenHeight, 0, 110, 200f))
+    private var backgrounds: ArrayList<Background>
+    private var obstacles: ArrayList<Obstacle>
 
     @Volatile
     private var running: Boolean = false
     private var gameThread: Thread? = null
 
     // For drawing
-    private val paint: Paint = Paint()
+    private val paint: Paint
     private var canvas: Canvas? = null
-    private val ourHolder: SurfaceHolder = holder
+    private val ourHolder: SurfaceHolder
+
+
+    private var owlBitmap : Bitmap
+    private var owlWidth : Int
+    private var owlHeight : Int
 
     // Control the fps
     private var fps: Long = 60
 
+    private var level : Level = Level.MIDDLE
+
+    fun updateLevel(level : Level){ this.level = level}
+
+
+
+    init {
+
+        // Initialize our drawing objects
+        ourHolder = holder
+        // ourForegroundHolder = holder
+        paint = Paint()
+
+        // Make a resource id out of the string of the file name
+        val resID = context.resources.getIdentifier("owl", "drawable", context.packageName)
+        owlBitmap = BitmapFactory.decodeResource(context.resources, resID)
+
+        // Initialize our array list
+        backgrounds = ArrayList()
+        obstacles = ArrayList()
+
+        val top = 0
+        val middle = (screenHeight*(1/3.toDouble())).toInt()
+        val bottom = (screenHeight*(2/3.toDouble())).toInt()
+
+        owlBitmap = Bitmap.createScaledBitmap(owlBitmap,
+            (((screenHeight/6.toDouble()).toInt()*owlBitmap.width)/(owlBitmap.height)), (screenHeight/6.toDouble()).toInt(), true)
+
+        owlWidth = owlBitmap.width
+        owlHeight = owlBitmap.height
+
+
+        backgrounds.add(Background(this.context, screenWidth, screenHeight,0, 110, 200f))
+
+        // Examples of how to add them, obviously we dont want to add them all at once. When they are off the screen they get removed.
+        obstacles.add(Obstacle(this.context, screenWidth, screenHeight, "drone", 0, 110, 200f, 3.0F, middle))
+        obstacles.add(Obstacle(this.context, screenWidth, screenHeight, "tree", 0, 110, 200f, 1.5F, middle))
+        obstacles.add(Obstacle(this.context, screenWidth, screenHeight, "branch", 0, 110, 200f, 3.0F, top))
+        obstacles.add(Obstacle(this.context, screenWidth, screenHeight, "fern", 0, 110, 200f, 3.0F, bottom))
+    }
+
     override fun run() {
+
         while (running) {
             val startFrameTime = System.currentTimeMillis()
 
             update()
 
             draw()
+            drawOwl()
+            drawObstacles()
 
             // Calculate the fps this frame
             val timeThisFrame = System.currentTimeMillis() - startFrameTime
             if (timeThisFrame >= 1) {
-                fps = 1000 / timeThisFrame
+                fps = (1000 / timeThisFrame)
             }
         }
     }
@@ -57,7 +100,9 @@ class SideScrollView internal constructor(context: Context, screenWidth: Int, sc
         } catch (e: InterruptedException) {
             // Error
         }
+
     }
+
 
     // Make a new thread and start it
     // Execution moves to our run method
@@ -85,7 +130,59 @@ class SideScrollView internal constructor(context: Context, screenWidth: Int, sc
 
         canvas!!.drawBitmap(bg.bitmap, fromRect1, toRect1, paint)
         canvas!!.drawBitmap(bg.bitmap, fromRect2, toRect2, paint)
+
     }
+
+
+    private fun drawObstacle(obstacle: Obstacle) {
+
+        // left   The X coordinate of the left side of the rectangle
+        // top    The Y coordinate of the top of the rectangle
+        // right  The X coordinate of the right side of the rectangle
+        // bottom The Y coordinate of the bottom of the rectangle
+        val leftSrc : Int
+        val rightSrc : Int
+
+        val leftDst : Int
+        val rightDst : Int
+        val topDst : Int
+        val bottomDst : Int
+
+        // Make a copy of the relevant background
+        if((obstacle.positionX + obstacle.width) > screenWidth) {
+            // clip right side of obstacle
+            leftSrc = 0
+            rightSrc = (obstacle.width - ((obstacle.positionX+obstacle.width)-screenWidth))
+
+            leftDst = obstacle.positionX
+            rightDst = screenWidth
+            topDst = obstacle.positionY
+            bottomDst = obstacle.positionY + obstacle.height
+        } else if (obstacle.positionX < 0) {
+            // clip left side of obstacle
+            leftSrc = (-obstacle.positionX)
+            rightSrc = obstacle.width
+
+            leftDst = 0
+            rightDst = (obstacle.width + obstacle.positionX)
+            topDst = obstacle.positionY
+            bottomDst = obstacle.positionY + obstacle.height
+        } else {
+            leftSrc = 0
+            rightSrc = obstacle.width
+
+            leftDst = obstacle.positionX
+            rightDst = obstacle.positionX + obstacle.width
+            topDst = obstacle.positionY
+            bottomDst = obstacle.positionY + obstacle.height
+        }
+        val fromRect1 = Rect(leftSrc, 0, rightSrc, obstacle.height)
+        val toRect1 = Rect(leftDst, topDst, rightDst, bottomDst)
+        canvas!!.drawBitmap(obstacle.bitmap, fromRect1, toRect1, paint)
+
+    }
+
+
 
     /**
      * Update all backgrounds (allows for multiple backgrounds)
@@ -94,6 +191,48 @@ class SideScrollView internal constructor(context: Context, screenWidth: Int, sc
         // Update all the background positions
         for (bg in backgrounds) {
             bg.update(fps)
+        }
+        removeOldObstacles()
+        for (obstacle in obstacles) {
+            obstacle.update(fps)
+        }
+    }
+
+    private fun removeOldObstacles(){
+        var temp : ArrayList<Obstacle> = ArrayList()
+        obstacles.forEach { obstacle -> if (obstacle.positionX > (-obstacle.width)) temp.add(obstacle) else obstacle.positionX = screenWidth }
+        obstacles = temp
+    }
+
+    private fun drawObstacles() {
+        if (holder.surface.isValid) {
+            for (obstacle in obstacles) {
+                drawObstacle(obstacle)
+            }
+            ourHolder.unlockCanvasAndPost(canvas)
+        }
+    }
+
+    private fun drawOwl(){
+        if (holder.surface.isValid) {
+            val leftDst = (screenWidth * 0.5).toInt() - (owlWidth * 0.5).toInt()
+            val rightDst = (screenWidth * 0.5).toInt() + (owlWidth * 0.5).toInt()
+            val topDst : Int
+            val bottomDst : Int
+            if (level == Level.MIDDLE){
+                topDst = (screenHeight * 0.5).toInt() - (owlHeight * 0.5).toInt()
+                bottomDst = (screenHeight * 0.5).toInt() + (owlHeight * 0.5).toInt()
+            } else if (level == Level.BOTTOM){
+                topDst = screenHeight - owlHeight
+                bottomDst = screenHeight
+            } else {
+                topDst = 0
+                bottomDst = owlHeight
+            }
+
+            val fromRect1 = Rect(0, 0, owlWidth, owlHeight)
+            val toRect1 = Rect(leftDst, topDst, rightDst, bottomDst)
+            canvas!!.drawBitmap(owlBitmap, fromRect1, toRect1, paint)
         }
     }
 
@@ -104,9 +243,8 @@ class SideScrollView internal constructor(context: Context, screenWidth: Int, sc
         if (ourHolder.surface.isValid) {
 
             canvas = ourHolder.lockCanvas()
-            canvas!!.drawColor(Color.argb(255, 0, 3, 70))
             drawBackground(0)
-            ourHolder.unlockCanvasAndPost(canvas)
+
         }
     }
 }
