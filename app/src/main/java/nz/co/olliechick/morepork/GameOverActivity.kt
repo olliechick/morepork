ec7bae86dc17f4687a4515dae654ab5a3c7b2ce8
Cameron Auld <cga51@uclive.ac.nz>
2019-05-27 23:52:22 +1200

    Make end of game view - Make buttons in end of game view functional

--- app/src/main/java/nz/co/olliechick/morepork/GameOverActivity.kt
+++ app/src/main/java/nz/co/olliechick/morepork/GameOverActivity.kt
@@ -1,12 +1,31 @@
 package nz.co.olliechick.morepork
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class GameOverActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_over)
+
+        // get reference to buttons
+        val playButton = findViewById<Button>(R.id.play_again_button)
+        val homeButton = findViewById<Button>(R.id.home_button)
+
+        // set on-click listeners
+        playButton.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+        homeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
