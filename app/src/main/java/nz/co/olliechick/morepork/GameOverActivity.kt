813fd3c98b8cf3ec911b2afe507ef183f1513798
Cameron Auld <cga51@uclive.ac.nz>
2019-06-04 22:35:28 +1200

    Added button to game over view that allows users to share their score via implicit intent - Also added the link to the app in the play store but it is plain text

--- app/src/main/java/nz/co/olliechick/morepork/GameOverActivity.kt
+++ app/src/main/java/nz/co/olliechick/morepork/GameOverActivity.kt
@@ -7,10 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.app.ShareCompat
 
 class GameOverActivity : AppCompatActivity() {
     private val PREFS_NAME = "scores"
     private val HIGH_SCORE = "highscore"
+    private val PLAY_STORE_ADDRESS = "https://play.google.com/store/apps/details?id=nz.co.olliechick.morepork"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +24,7 @@ class GameOverActivity : AppCompatActivity() {
         // get reference to buttons
         val playButton = findViewById<Button>(R.id.play_again_button)
         val homeButton = findViewById<Button>(R.id.home_button)
+        val shareButton = findViewById<Button>(R.id.share_button)
 
         val score = intent.getIntExtra("score", 0)
         var yourScore = findViewById<TextView>(R.id.your_score_textview).setText(score.toString())
@@ -48,6 +51,20 @@ class GameOverActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        // Allows the user to share their score and a link to the app
+        shareButton.setOnClickListener { view ->
+            val shareIntent = ShareCompat.IntentBuilder
+                .from(this)
+                .setType("text/plain")
+                .setChooserTitle("Share")
+                .setText("I got a high score of $score on Morepork. Can you beat it?\n $PLAY_STORE_ADDRESS")
+                .intent
+
+            if (shareIntent.resolveActivity(this.packageManager) != null) {
+                this.startActivity(shareIntent)
+            }
         }
     }
 
