50d818c4ee66574e8c9053ce734d670180ed1f84
Ollie Chick <och26@uclive.ac.nz>
2019-06-04 18:08:09 +1200

    Use sound barrier as stored in SharedPreferences

--- app/src/main/java/nz/co/olliechick/morepork/GameActivity.kt
+++ app/src/main/java/nz/co/olliechick/morepork/GameActivity.kt
@@ -1,27 +1,18 @@
 package nz.co.olliechick.morepork
 
-import android.Manifest
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Toast
-import android.hardware.SensorEvent
-import android.hardware.SensorManager
-import android.hardware.Sensor
-import android.hardware.SensorEventListener
-import android.os.Handler
-import androidx.core.app.ActivityCompat
-import android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
-import android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 import android.graphics.Point
-import android.view.Gravity
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.os.Handler
+import android.view.View.*
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
 
 
 class GameActivity : AppCompatActivity(), SensorEventListener {
@@ -101,12 +92,14 @@ class GameActivity : AppCompatActivity(), SensorEventListener {
         soundMeter!!.start()
         val handler = Handler()
         val delay = (1000 / CHECK_FREQUENCY).toLong() //milliseconds
+        val soundBarrier = PreferenceManager.getDefaultSharedPreferences(this)
+            .getInt("sound_level_limit", 2000)
 
         handler.postDelayed(object : Runnable {
             override fun run() {
                 val soundLevel = soundMeter?.amplitude
                 if (soundLevel != null) {
-                    if (soundLevel > SOUND_BARRIER) loudNoise()
+                    if (soundLevel > soundBarrier) loudNoise()
                     else quietNoise()
                 }
                 if (!sideScrollView!!.running) {
